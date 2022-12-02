1d528f8e5fe5aab10376bf106cbad7251a64fc44
rubydusa <rubydusa@protonmail.com>
2022-12-02 14:12:59 +0200

    card design

--- front/src/components/NFTViewElementModal.js
+++ front/src/components/NFTViewElementModal.js
@@ -9,8 +9,31 @@ export default function NFTViewElementModal({src}) {
       src !== null 
         ? <>
             <div className='portal-root'>
-              <div className='portal-element-container'>
-                <SVG src={src} />
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '30vw',
+                height: '80vh',
+                marginTop: '6vh',
+                padding: '2vw',
+                backgroundColor: 'white',
+                borderRadius: '10px',
+              }}>
+                <div className='portal-element-container' style={{
+                  flex: 2,
+                  display: 'flex',
+                  overflow: 'auto',
+                }}>
+                  <SVG src={src} />
+                </div>
+                <p style={{
+                  flex: 1,
+                  display: 'flex',
+                  overflow: 'auto',
+                }}>
+                  Occaecat cupidatat cupidatat nostrud pariatur id culpa ad dolore sit non magna. Qui consectetur est irure dolor aute aliqua. Consequat exercitation duis in veniam exercitation. Amet ipsum esse dolore excepteur labore amet exercitation pariatur sunt sint. Non cupidatat voluptate minim in reprehenderit est voluptate. Pariatur veniam pariatur et do est. 
+                </p>
               </div>
             </div> 
             <div className='portal-background'>
