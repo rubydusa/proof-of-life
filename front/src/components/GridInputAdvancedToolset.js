bd867346e7dbd241464afb0bdef56848db2585c4
rubydusa <rubydusa@protonmail.com>
2022-12-22 20:34:28 +0200

    advanced toolset advancements

--- front/src/components/GridInputAdvancedToolset.js
+++ front/src/components/GridInputAdvancedToolset.js
@@ -7,20 +7,45 @@ import GlobalContext from '../data/global'
 export default function GridInputAdvancedToolset({grid, gridInput, setGridInput, flush}) {
   const { GRID_SETTINGS } = useContext(GlobalContext);
   return (
-    <div>
+    <div className='grid-input-advanced-toolset'>
+      <h2>
+        Advanced Toolset
+      </h2>
       <button 
         className='btn'
+        style={{
+          width: '80%'
+        }}
         onClick={() => flush(numToGrid(gridInput, GRID_SETTINGS))}>
         Load
       </button>
       <button 
         className='btn'
+        style={{
+          width: '80%'
+        }}
         onClick={() => setGridInput(gridToNum(grid))}>
         Save
       </button>
+      <div style={{
+        width: '100%',
+        fontSize: '16px',
+        paddingLeft: '12px',
+        boxSizing: 'border-box',
+      }}>
+        Grid Number:
+      </div>
       <input
         type="text"
+        style={{
+          border: '2px solid var(--gray)',
+          borderRadius: '0.5em',
+          outline: 'none',
+          padding: '8px',
+          boxSizing: 'border-box',
+        }}
         value={gridInput}
+        placeholder={'Grid Number'}
         onChange={(e) => setGridInput(e.target.value)}/>
     </div>
   )
