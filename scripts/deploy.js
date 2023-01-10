ed245da86ce86286a97addbdf1d2eacb608b6017
rubydusa <rubydusa@protonmail.com>
2023-01-10 22:22:16 +0200

    new test deploy with metadata

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@ const H = "8";
 const EXPIRY = "900";
 
 // on average, P / 2 ** BITS_OUT_OF alive cells in a random board generation 
-const P = "5";
+const P = "3";
 const BITS_OUT_OF = "6";
 
 function sleep(ms) {
@@ -20,20 +20,23 @@ async function main() {
     const [deployerAccount] = await ethers.getSigners();
 
     const GOLNFT = await ethers.getContractFactory("GOLNFT", deployerAccount);
-    const GOLVerifier = await hre.circom.getVerifierFactory("Main3N8x8");
+    const GOLVerifier = await hre.circom.getVerifierFactory("Main3N8x8", deployerAccount);
     const GOLSVG = await ethers.getContractFactory("GOLSVG", deployerAccount);
+    const GOLMetadata = await ethers.getContractFactory("GOLMetadata", deployerAccount);
     const GOLRandom = await ethers.getContractFactory("GOLRandom", deployerAccount);
     
     const preBalance = await deployerAccount.getBalance();
 
-    await sleep(5000);
     const golRandom = await GOLRandom.deploy(P, BITS_OUT_OF);
-    await sleep(5000);
+    await golRandom.deployTransaction.wait();
     const golSVG = await GOLSVG.deploy();
-    await sleep(5000);
+    await golSVG.deployTransaction.wait();
+    const golMetadata = await GOLMetadata.deploy("https://medium.com/coinmonks/proof-of-life-zero-knowledge-proof-implementation-of-conways-game-of-life-with-circom-and-6438521fb2b1");
+    await golMetadata.deployTransaction.wait();
     const golVerifier = await GOLVerifier.deploy();
-    await sleep(5000);
-    const golNFT = await GOLNFT.deploy(golVerifier.address, golRandom.address, golSVG.address, W, H, EXPIRY); 
+    await golVerifier.deployTransaction.wait();
+    const golNFT = await GOLNFT.deploy(golVerifier.address, golRandom.address, golSVG.address, golMetadata.address, W, H, EXPIRY);
+    await golNFT.deployTransaction.wait();
     
     const postBalance = await deployerAccount.getBalance();
     
@@ -46,6 +49,9 @@ async function main() {
             },
             golSVG: {
                 address: golSVG.address
+            },
+            golMetadata: {
+                address: golMetadata.address
             },
             golVerifier: {
                 address: golVerifier.address
